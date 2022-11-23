b2bcadc6b1b4a0d0a48cf8b49c1d815c2f295669
kaminyv <kaminyv@yandex.ru>
2022-11-24 00:26:48 +0600

    Update

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import concurrent
 from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
-from typing import NamedTuple, Optional, Dict, List
-from urllib.parse import urlparse
-
+from typing import NamedTuple, Optional, List
+from urllib.parse import urlparse, ParseResult
 
 import requests
 import re
-
+from requests import Response
 
 TEST_ONCE_DOMAIN = [
     'http://google.com/',
 ]
 
 TEST_MANY_DOMAINS = [
-    'userbenchmark.com',
-    'humaverse.com',
-    'homeaway.com',
-    'google.com',
-    'facebook.com',
-    'youtube.com',
-    'twitter.com',
-    'instagram.com',
-    'linkedin.com',
-    'apple.com',
-    'microsoft.com',
-    'wikipedia.org',
-    'wordpress.org',
-    'googletagmanager.com',
-    'hibid.com',
-    'laopinion.com',
+    'http://userbenchmark.com/fsfdaf',
+    'http://humaverse.com',
+    'http://homeaway.com',
+    'https://google.com/fsdfdsaaa/dfsasf',
+    'http://facebook.com',
+    'http://youtube.com',
+    'http://twitter.com',
+    'http://instagram.com',
+    'http://linkedin.com',
+    'http://apple.com',
+    'http://microsoft.com',
+    'http://wikipedia.org',
+    'http://wordpress.org',
+    'http://googletagmanager.com',
+    'http://hibid.com',
+    'http://laopinion.com',
 ]
 
 
@@ -44,7 +43,7 @@ class Status(Enum):
 class AdsTxtResponse(NamedTuple):
     status: Status
     domain: str
-    data: Optional[Dict] = None
+    data: Optional[List] = None
     error: Optional[Exception] = None
 
 
@@ -54,18 +53,21 @@ class IncorrectInputError(Exception):
 
 class CrawlerAdsTxt:
     @staticmethod
-    def __validate_domain(domain: str) -> str:
-        if not domain:
+    def __validate_input(input_str: str) -> str:
+        pattern = r'^(https?\:\/\/)?((?:[-a-zа-яё0-9]{1,63}\.)' \
+                  r'?([-a-zа-яё0-9]{1,63})\.[a-za-яё]{2,6}){1,255}'
+
+        match = re.match(pattern, input_str)
+
+        if match is None:
             raise IncorrectInputError('Invalid domain')
 
-        domain_parse = urlparse(domain)
-        domain = domain_parse.netloc if not domain_parse.path else domain_parse.path
-
-        return domain
+        return match.group(0)
 
     def __create_url(self, domain: str) -> str:
-        domain = self.__validate_domain(domain)
-        url = f'http://{domain}/ads.txt'
+        domain = self.__validate_input(domain)
+        url = f"{domain}/ads.txt" if re.match(r'^https?', domain) \
+            else f'http://{domain}/ads.txt'
 
         return url
 
@@ -75,48 +77,51 @@ class CrawlerAdsTxt:
         return response
 
     @staticmethod
-    def __is_signature_found(signature: str, text: str):
+    def __find_signature(signature: str, text: str) -> list:
         signature = signature.split('|')
         name_signature = signature[0]
         type_signature = signature[1]
-        pattern = name_signature + '.+' + type_signature
-        search = re.search(pattern, text, re.IGNORECASE)
+        pattern = f'{name_signature}.+{type_signature}'
 
-        if search is not None:
-            return True
+        search = re.findall(pattern, text, re.IGNORECASE)
 
-        return False
+        return search
 
-    def __find_signature_by_domain(self, domain_name, signature, timeout):
+    def _crawl(self, domain_name, timeout) -> Response:
+        url = self.__create_url(domain_name)
+        response = self.__request(url, timeout)
+        return response
+
+    def _find(self, domain_name, signature, timeout) -> AdsTxtResponse:
         try:
-            url = self.__create_url(domain_name)
+            response = self._crawl(domain_name, timeout)
         except IncorrectInputError as e:
             return AdsTxtResponse(status=Status.INCORRECT_INPUT, error=e, domain=domain_name)
-
-        response = self.__request(url, timeout)
 
         if response.status_code != 200:
             return AdsTxtResponse(status=Status.FAILED_RESPONSE, domain=domain_name)
 
-        signature_found = self.__is_signature_found(signature, response.text)
-        if signature_found:
+        found_signature = self.__find_signature(signature, response.text)
+
+        if found_signature:
             return AdsTxtResponse(
                 status=Status.SUCCESS,
-                data={'signature': signature},
-                domain=domain_name
-            )
-        else:
-            return AdsTxtResponse(
-                status=Status.SIGNATURE_NOT_FOUND,
-                data={'signature': signature},
+                data=found_signature,
                 domain=domain_name
             )
 
-    def find_adstxt(self, domains: list, signature: str, threads: int = 5, timeout: int = 3) -> List[AdsTxtResponse]:
+        return AdsTxtResponse(
+            status=Status.SIGNATURE_NOT_FOUND,
+            data=found_signature,
+            domain=domain_name
+        )
+
+    def find(self, domains: list, signature: str,
+             threads: int = 5, timeout: int = 3) -> List[AdsTxtResponse]:
         """
         Search for signature occurrences in ADS.TXT file
 
-        :param domains: domain list
+        :param domains: domain list (http://example.com)
         :param signature: example: 'google.com|direct' or 'google.com|reseller'
         :param threads: number of threads
         :param timeout: the waiting time for a response from the domain
@@ -125,7 +130,8 @@ class CrawlerAdsTxt:
 
         with ThreadPoolExecutor(max_workers=threads) as executor:
             future_to_domain = {
-                executor.submit(self.__find_signature_by_domain, domain, signature, timeout): domain
+                executor.submit(self._find, domain,
+                                signature, timeout): domain
                 for domain in domains
             }
             for future in concurrent.futures.as_completed(future_to_domain):
@@ -134,11 +140,13 @@ class CrawlerAdsTxt:
                     data = future.result()
                     responses.append(data)
                 except Exception as exc:
-                    responses.append(AdsTxtResponse(status=Status.INTERNAL_ERROR, error=exc, domain=domain))
+                    responses.append(AdsTxtResponse(
+                        status=Status.INTERNAL_ERROR,
+                        error=exc, domain=domain))
 
         return responses
 
 
 if __name__ == '__main__':
-    # print(CrawlerAdsTxt._CrawlerAdsTxt__validate_domain('dff.google.com'))
-    print(CrawlerAdsTxt().find_adstxt(TEST_MANY_DOMAINS, 'google.com|DIRECT'))
+    # print(CrawlerAdsTxt._CrawlerAdsTxt__validate_domain('.com/fsdfdsaaa/dfsasf'))
+    print(CrawlerAdsTxt().find(TEST_MANY_DOMAINS, 'google.com|DIRECT'))
